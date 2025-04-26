23ea458c93f484ab84f2631a9dde2ababe6a494f
Limoncello-Zoey <limoncellow@163.com>
2025-04-26 17:44:06 +0800

    完善等待大厅

--- Classes/WaitingHall.cpp
+++ Classes/WaitingHall.cpp
@@ -7,6 +7,17 @@ bool WaitingHall::init()
 {
 	if (!Scene::init()) return false;
 
+	auto VisibleSize = Director::getInstance()->getVisibleSize();
+	auto origin = Director::getInstance()->getVisibleOrigin();
+
+	auto CreateButton = Button::create("CreateRoomNormal.png", "CreateRoomSelected.png");
+	auto JoinButton = Button::create("JoinRoomNormal.png", "JoinRoomSelected.png");
+
+	CreateButton->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 + 50));
+	JoinButton->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 - 50));
+
+	CreateButton->addTouchEventListener(CC_CALLBACK_2(WaitingHall::onIamHostClicked, this));
+	
 }
 
 Scene* WaitingHall::createScene()
