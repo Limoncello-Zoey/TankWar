d90e6640b0515cee80778adcabf5f6b95bd93cb0
Limoncello-Zoey <limoncellow@163.com>
2025-05-18 23:09:28 +0800

    修改UI

--- Classes/WaitingHall.cpp
+++ Classes/WaitingHall.cpp
@@ -17,31 +17,20 @@ bool WaitingHall::init()
 	// add buttons
 	auto CreateButton = Button::create("CreateRoomNormal.png", "CreateRoomSelected.png");
 	auto JoinButton = Button::create("JoinRoomNormal.png", "JoinRoomSelected.png");
-	auto DemoButton = Button::create("DemoNormal.png", "DemoSelected.png");
+	//auto DemoButton = Button::create("DemoNormal.png", "DemoSelected.png");
 
 	CreateButton->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 + 100-300));
 	JoinButton->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 - 100-300));
-	DemoButton->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 + 300 - 300));
+	//DemoButton->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 + 300 - 300));
 
 	CreateButton->addTouchEventListener(CC_CALLBACK_2(WaitingHall::onCreateRoomClicked, this));
 	JoinButton->addTouchEventListener(CC_CALLBACK_2(WaitingHall::onJoinRoomClicked, this));
-	DemoButton->addTouchEventListener(CC_CALLBACK_2(WaitingHall::onDemoClicked, this));
+	//DemoButton->addTouchEventListener(CC_CALLBACK_2(WaitingHall::onDemoClicked, this));
 
 	this->addChild(CreateButton);
 	this->addChild(JoinButton);
-	this->addChild(DemoButton);
+	//this->addChild(DemoButton);
 
-	//add output bar
-	auto outputbar = Label::createWithTTF(
-		"这是一段长文本，当内容超过指定宽度时会自动换行。",
-		"fonts/Marker Felt.ttf", 24,
-		Size(400, 0), // 宽度固定为400，高度自适应
-		TextHAlignment::LEFT
-	);
-	outputbar->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 ));
-	outputbar->setTextColor(Color4B::WHITE);
-	outputbar->setOpacity(255);
-	this->addChild(outputbar,-2);
 
 
 	// Create labels for buttons
@@ -50,12 +39,12 @@ bool WaitingHall::init()
 	//label1->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 + 100 - 200));
 	//auto label2 = Label::createWithTTF("Join Room", "fonts/Marker Felt.ttf", 24);
 	//label2->setPosition(Vec2(VisibleSize.width / 2, VisibleSize.height / 2 - 100 - 200));
-	auto label1 = Label::createWithTTF("Create Room", "fonts/Marker Felt.ttf", 48);
-	label1->setPosition(Vec2(0, 0));
-	auto label2 = Label::createWithTTF("Join Room", "fonts/Marker Felt.ttf", 48);
-	label2->setPosition(Vec2(0, 0));
-	CreateButton->addChild(label1);
-	JoinButton->addChild(label2);
+	//auto label1 = Label::createWithTTF("Create Room", "fonts/Marker Felt.ttf", 48);
+	//label1->setPosition(Vec2(0, 0));
+	//auto label2 = Label::createWithTTF("Join Room", "fonts/Marker Felt.ttf", 48);
+	//label2->setPosition(Vec2(0, 0));
+	//CreateButton->addChild(label1);
+	//JoinButton->addChild(label2);
 
 	// create background
 	//auto background = Sprite::create("background.png");
